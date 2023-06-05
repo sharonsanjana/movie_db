7c1e08e36869b44288f5b9d187e6c831279ccb1d
sharonsanjana <sschintu@gmail.com>
2023-06-05 08:49:06 +0530

    movie db is updated again

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 require("dotenv").config()
 
-
 const express = require("express")
 const app = express()
 const PORT = 5678
 const movie = require("./routes/movie")
 const mongoose = require('mongoose')
-
 
 mongoose.connect(process.env.DB_URL)
 const db= mongoose.connection
@@ -18,7 +16,7 @@ app.get('/',(request,response) => {
     response.send("hello")
 
 })
-
+ 
 app.use('/api/v1/movie',movie) 
 
 app.listen(PORT,console.log(`database running at http://localhost:${PORT}`))
